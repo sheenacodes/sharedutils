028379bbfd81a64c584332314c21d1c720a1d42f
Sheena Puthan <sheena.puthan@gmail.com>
2024-09-09 12:38:22 +0300

    redish hash HGET utility

--- redis/redis_hash.go
+++ redis/redis_hash.go
@@ -2,11 +2,14 @@ package redis
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/redis/go-redis/v9"
 	"github.com/sheenacodes/sharedutils/logger"
 )
 
-func (r *RedisClient) AddFieldValueToHash(hashKey string, fieldName string, fieldValue string) error {
+func (r *RedisClient) AddFieldToHash(hashKey string, fieldName string, fieldValue time.Time) error {
 	ctx := context.Background()
 	err := r.Client.HSet(ctx, hashKey, fieldName, fieldValue).Err()
 	if err != nil {
@@ -15,4 +18,25 @@ func (r *RedisClient) AddFieldValueToHash(hashKey string, fieldName string, fiel
 	}
 	logger.Log.Debug().Msgf(" %s:%s added to Redis Hash for key %s", fieldName, fieldValue, hashKey)
 	return nil
+}
+
+func (r *RedisClient) GetFieldAsTime(hashKey string, fieldName string, layout string) (time.Time, error) {
+	// Use HGet to retrieve the field value
+	ctx := context.Background()
+	value, err := r.Client.HGet(ctx, hashKey, fieldName).Result()
+	if err != nil {
+		if err == redis.Nil {
+			// The field does not exist
+			return time.Time{}, fmt.Errorf("field %s does not exist in hash %s", fieldName, hashKey)
+		}
+		return time.Time{}, fmt.Errorf("failed to get field value: %v", err)
+	}
+
+	// Parse the value as a time.Time
+	parsedTime, err := time.Parse(layout, value)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("failed to parse time: %v", err)
+	}
+
+	return parsedTime, nil
 }
